5141da781431859eab8d45415b51f1bafbb9d560
CHARAN KUMAR UDAYAGIRI <67089878+UDAYAGIRICHARAN@users.noreply.github.com>
2022-01-28 11:50:37 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,4 +128,3 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals())
